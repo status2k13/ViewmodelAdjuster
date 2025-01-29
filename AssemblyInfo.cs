9d1ac866fb82f1c1930ae7cc8893068149f8f044
huutista <jepulis@eiollu.com>
2025-01-29 05:51:28 +0200

    Add project files.

---
+++ AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Viewmodel Adjuster")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("meanw")]
+[assembly: AssemblyProduct("Viewmodel Adjuster")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("9076ade2-f4ee-4b5d-b9a6-29a054cf0a7e")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Plugin.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using Comfort.Common;
+using EFT;
+
+namespace ViewmodelAdjuster
+{
+    [BepInPlugin("com.meanw.viewmodeladjuster", "Viewmodel Adjuster", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static ConfigEntry<float> vmx;
+        internal static ConfigEntry<float> vmy;
+        internal static ConfigEntry<float> vmz;
+
+        protected void Awake()
+        {
+            vmx = Config.Bind("MAIN", "VIEWMODEL_X", 0.04f, new ConfigDescription("", new AcceptableValueRange<float>(-0.1f, 0.1f)));
+            vmy = Config.Bind("MAIN", "VIEWMODEL_Y", 0.04f, new ConfigDescription("", new AcceptableValueRange<float>(-0.1f, 0.1f)));
+            vmz = Config.Bind("MAIN", "VIEWMODEL_Z", 0.05f, new ConfigDescription("", new AcceptableValueRange<float>(-0.1f, 0.1f)));
+        }
+
+        protected void Update()
+        {
+            var gameWorld = Singleton<GameWorld>.Instance;
+            if (gameWorld?.RegisteredPlayers == null || gameWorld.MainPlayer?.ProceduralWeaponAnimation?.HandsContainer == null)
+            {
+                return;
+            }
+
+            gameWorld.MainPlayer.ProceduralWeaponAnimation.HandsContainer.CameraOffset.x = vmx.Value;
+            gameWorld.MainPlayer.ProceduralWeaponAnimation.HandsContainer.CameraOffset.y = vmy.Value;
+            gameWorld.MainPlayer.ProceduralWeaponAnimation.HandsContainer.CameraOffset.z = vmz.Value;
+        }
+    }
+}
